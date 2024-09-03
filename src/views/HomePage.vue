649f96adc80b1dab9ac775f23b816aa9450206a1
belladev250 <lissadev17@gmail.com>
2024-09-03 21:50:02 +0200

    added another two sections

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,177 +1,203 @@
-<template>
-  <div class="overflow-x-hidden">
-    <div class="h-screen">
-      <div class="relative h-[96%]">
-        <div class="absolute inset-0 bg-black opacity-30"></div>
-        <img src="../assets/bghome.jpg" alt="" class="h-full w-full">
-        <NavBar class="absolute top-0 left-0 w-full" />
+  <template>
+    <div class="overflow-x-hidden">
+      <div class="h-screen">
+        <div class="relative h-[96%]">
+          <div class="absolute inset-0 bg-black opacity-30"></div>
+          <img src="../assets/bghome.jpg" alt="" class="h-full w-full">
+          <NavBar class="absolute top-0 left-0 w-full" />
 
-        <div class="font-rockinsoda absolute text-white text-6xl top-56 ml-12">
-          <p>THE LARGEST IN-PERSON</p>
-          <p>TECHNOLOGY EVENT IN</p>
-          <p>Rwanda</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="absolute top-2/3 ml-12">
-      <div class="flex">
-        <button class="mr-8 p-2 px-4 font-gramatika bg-white text-black font-semibold hover:bg-tangerine hover:text-white">
-          Apply Now
-        </button>
-        <button class="p-2 px-4 font-gramatika border px-8 text-white font-semibold hover:bg-tangerine hover:text-black">
-          Impact video
-        </button>
-      </div>
-      <img src="../assets/gvticons.svg" class="mt-12">
-    </div>
-
-    <!-- First section -->
-    <div class="mt-16 ml-12">
-      <p class="font-rockinsoda text-navy text-7xl">IGNITING AND INSPIRING</p>
-      <p class="font-rockinsoda text-navy text-7xl">INNOVATION</p>
-    </div>
-
-    <!-- Second section -->
-    <div class="ml-12 flex mt-8 gap-4">
-      <div class="flex-2">
-        <img src="../assets/students.jpg" class="w-full h-auto object-cover">
-        <p class="font-gramatika mt-8 text-darknavy text-xl">
-          We believe in empowering innovators solving some of the dire
-        </p>
-        <p class="font-gramatika text-darknavy text-xl">
-          challenges in the Rwandan community and abroad
-        </p>
-        <button class="mr-8 mt-6 p-2 px-8 font-gramatika bg-tangerine text-white font-semibold">
-          Learn
-        </button>
-      </div>
-
-      <!-- Right side with vertically stacked images -->
-      <div class="flex-1 space-y-4">
-        <div class="mr-10">
-          <img src="../assets/speaker.png" class="h-60 w-96 object-cover">
-        </div>
-        <div class="mr-10">
-          <img src="../assets/audience.jpg" class="h-60 w-96 object-cover">
-        </div>
-      </div>
-    </div>
-
-    <!-- Third section -->
-    <div class="ml-12 flex mt-28 space-x-80">
-      <div class="space-y-8">
-        <img src="../assets/Component 1.png" class="">
-        <hr class="border-t border-gray-300">
-        <div class="-space-y-2">
-          <p class="font-rockinsoda text-7xl text-navy">130+</p>
-          <p class="text-lightblue font-gramatika">Startups</p>
+          <div class="font-rockinsoda absolute text-white text-6xl top-56 ml-12">
+            <p>THE LARGEST IN-PERSON</p>
+            <p>TECHNOLOGY EVENT IN</p>
+            <p>Rwanda</p>
+          </div>
         </div>
       </div>
 
-      <div class="space-y-8">
-        <img src="../assets/Component 1 (2).png"  class="ml-8">
-        <hr class="border-t border-gray-300">
-        <div class="-space-y-2">
-          <p class="font-rockinsoda text-7xl text-navy">10,000+</p>
-          <p class="text-lightblue font-gramatika">Attendees</p>
-        </div>
-      </div>
-
-      <div class="space-y-8">
-        <img src="../assets/Component 1 (1).png" class="ml-16">
-        <hr class="border-t border-gray-300">
-        <div class="-space-y-2">
-          <p class="font-rockinsoda text-7xl text-navy">RWF600M</p>
-          <p class="text-lightblue font-gramatika">Invested in tech startups</p>
-        </div>
-      </div>
-    </div>
-
-      <!-- Fourth section -->
-      <div class="relative h-[600px]"> 
-        <div class="absolute text-white p-8 mt-20">
-          <img src="../assets/notched.svg" alt="" class="w-full">
-          <img src="../assets/Vector 6.png" class="absolute top-0 mt-24 left-0 w-1/3 h-auto left-[57%]">
-        </div>
-  
-        <div class="absolute mt-72 ml-12 p-8 text-left">
-          <h2 class="text-6xl text-white font-rockinsoda mb-4">Pitch Your Idea for</h2>
-          <h2 class="text-6xl text-white font-rockinsoda mb-4">Funding</h2>
-          <p class="text-lg mb-1 text-white text-xl font-gramatika">
-            The largest in-person technology event in Rwanda
-          </p>
-          <p class="text-lg mb-1 text-white text-xl font-gramatika">
-            designed to ignite & inspire innovation, Hanga
-          </p>
-          <p class="text-lg mb-6 text-white text-xl font-gramatika">
-            Pitchfest is returning for its fourth edition.
-          </p>
-          <button class="bg-tangerine text-white py-2 px-6 hover:bg-orange-500">
-            Learn more
+      <div class="absolute top-2/3 ml-12">
+        <div class="flex">
+          <button class="mr-8 p-2 px-4 font-gramatika bg-white text-black font-semibold hover:bg-tangerine hover:text-white">
+            Apply Now
+          </button>
+          <button class="p-2 px-4 font-gramatika border px-8 text-white font-semibold hover:bg-tangerine hover:text-black">
+            Impact video
           </button>
         </div>
+        <img src="../assets/gvticons.svg" class="mt-12">
       </div>
-  
-      <!-- Fifth section -->
-      <div class="ml-12 flex mt-72 gap-4"> 
-       
+
+      <!-- First section -->
+      <div class="mt-16 ml-12">
+        <p class="font-rockinsoda text-navy text-7xl">IGNITING AND INSPIRING</p>
+        <p class="font-rockinsoda text-navy text-7xl">INNOVATION</p>
+      </div>
+
+      <!-- Second section -->
+      <div class="ml-12 flex mt-8 gap-4">
+        <div class="flex-2">
+          <img src="../assets/students.jpg" class="w-full h-auto object-cover">
+          <p class="font-gramatika mt-8 text-darknavy text-xl">
+            We believe in empowering innovators solving some of the dire
+          </p>
+          <p class="font-gramatika text-darknavy text-xl">
+            challenges in the Rwandan community and abroad
+          </p>
+          <button class="mr-8 mt-6 p-2 px-8 font-gramatika bg-tangerine text-white font-semibold">
+            Learn
+          </button>
+        </div>
+
         <!-- Right side with vertically stacked images -->
         <div class="flex-1 space-y-4">
-          <div class="">
-            <img src="../assets/mrspeaker.jpg" class="h-60 w-96 object-cover">
+          <div class="mr-10">
+            <img src="../assets/speaker.png" class="h-60 w-96 object-cover">
           </div>
-          <div class="">
-            <img src="../assets/whiteman.jpg" class="h-60 w-96 object-cover">
+          <div class="mr-10">
+            <img src="../assets/audience.jpg" class="h-60 w-96 object-cover">
+          </div>
+        </div>
+      </div>
+
+      <!-- Third section -->
+      <div class="ml-12 flex mt-28 space-x-80">
+        <div class="space-y-8">
+          <img src="../assets/Component 1.png" class="">
+          <hr class="border-t border-gray-300">
+          <div class="-space-y-2">
+            <p class="font-rockinsoda text-7xl text-navy">130+</p>
+            <p class="text-lightblue font-gramatika">Startups</p>
           </div>
         </div>
 
-        <div class="flex-2 mr-10">
-          <img src="../assets/paula.jpg" class="w-full h-[69%] object-cover">
+        <div class="space-y-8">
+          <img src="../assets/Component 1 (2).png"  class="ml-8">
+          <hr class="border-t border-gray-300">
+          <div class="-space-y-2">
+            <p class="font-rockinsoda text-7xl text-navy">10,000+</p>
+            <p class="text-lightblue font-gramatika">Attendees</p>
+          </div>
+        </div>
+
+        <div class="space-y-8">
+          <img src="../assets/Component 1 (1).png" class="ml-16">
+          <hr class="border-t border-gray-300">
+          <div class="-space-y-2">
+            <p class="font-rockinsoda text-7xl text-navy">RWF600M</p>
+            <p class="text-lightblue font-gramatika">Invested in tech startups</p>
+          </div>
+        </div>
+      </div>
+
+        <!-- Fourth section -->
+        <div class="relative h-[600px]"> 
+          <div class="absolute text-white p-8 mt-20">
+            <img src="../assets/notched.svg" alt="" class="w-full">
+            <img src="../assets/Vector 6.png" class="absolute top-0 mt-24 left-0 w-1/3 h-auto left-[57%]">
+          </div>
+    
+          <div class="absolute mt-72 ml-12 p-8 text-left">
+            <h2 class="text-6xl text-white font-rockinsoda mb-4">Pitch Your Idea for</h2>
+            <h2 class="text-6xl text-white font-rockinsoda mb-4">Funding</h2>
+            <p class="text-lg mb-1 text-white text-xl font-gramatika">
+              The largest in-person technology event in Rwanda
+            </p>
+            <p class="text-lg mb-1 text-white text-xl font-gramatika">
+              designed to ignite & inspire innovation, Hanga
+            </p>
+            <p class="text-lg mb-6 text-white text-xl font-gramatika">
+              Pitchfest is returning for its fourth edition.
+            </p>
+            <button class="bg-tangerine text-white py-2 px-6 hover:bg-orange-500">
+              Learn more
+            </button>
+          </div>
+        </div>
+    
+        <!-- Fifth section -->
+        <div class="ml-12 flex mt-72 gap-4"> 
+        
+          <!-- Right side with vertically stacked images -->
+          <div class="flex-1 space-y-4">
+            <div class="">
+              <img src="../assets/mrspeaker.jpg" class="h-60 w-96 object-cover">
+            </div>
+            <div class="">
+              <img src="../assets/whiteman.jpg" class="h-60 w-96 object-cover">
+            </div>
+          </div>
+
+          <div class="flex-2 mr-10">
+            <img src="../assets/paula.jpg" class="w-full h-[69%] object-cover">
+          
+          </div>
+
+          
+        </div>
+
+        <!---------- section six------------ -->
+        <div class="ml-12 flex space-x-8 -mt-24 mb-12">
+          <div class="mt-32">
+            <h1 class="font-rockinsoda text-4xl text-navy">CHECK OUR INITIATIVES</h1>
+            <p class="font-gramatika text-xl text-navy mt-2">Lorem Ipsum has been the industry's standard 
+              dummy </p>
+              <p class="front-gramatika text-xl text-navy ">text ever since the 1500s,
+                when an unknown printer</p>
+          </div>
+
+          <div class="space-x-4 flex">
+            <div class="h-[70vh] w-[25vw] flex-shrink-0">
+              <img src="../assets/hubs.jpg" class="w-full h-full object-cover">
+            </div>
+            <div class="h-[70vh] w-[25vw] flex-shrink-0">
+              <img src="../assets/srs.jpg" class="w-full h-full object-cover">
+            </div>
+          </div>
+          
+        
+        </div>
+
+        <!--------section seven------->
+        <div class="h-[650px] mt-24 mb-3">
+          <img src="../assets/Group .jpg" alt="" class="w-full h-full object-cover">
+        </div>
+        
+        <!------ section eight ------->
+        <div class="space-y-12 mt-24">
+          <div class="ml-12">
+            <h1 class="font-rockinsoda text-5xl text-navy"> LATEST ON OUR BLOG</h1>
+            <p class="font-gramatika text-xl mt-4 text-lightblue">Get updates on the latest stories  within Rwanda’s </p>
+            <p class="font-gramatika text-xl text-lightblue ">flourishing tech innovation ecosystem. </p>
+            <button class="bg-tangerine float-right -mt-12 mr-10 text-white py-2 px-6 hover:bg-orange-500">
+             read more
+            </button>
+
+          </div>
+          
+          <div class="flex space-x-6 ml-12 mr-12">
+            <div class="h-[500px] w-[600px] flex-shrink-0"> <!-- Set a width to make the image wider -->
+              <img src="../assets/stad.jpg" class="w-full h-full object-cover">
+            </div>
+            <div class="h-[500px] w-[600px] flex-shrink-0"> <!-- Set the same width for consistency -->
+              <img src="../assets/vr.jpg" class="w-full h-full object-cover">
+            </div>
+          </div>
          
+
         </div>
 
-        
-      </div>
-
-      <!---------- section six------------ -->
-      <div class="ml-12 flex space-x-8 -mt-24 mb-12">
-        <div class="mt-32">
-          <h1 class="font-rockinsoda text-4xl text-navy">CHECK OUR INITIATIVES</h1>
-          <p class="font-gramatika text-xl text-navy mt-2">Lorem Ipsum has been the industry's standard 
-            dummy </p>
-            <p class="front-gramatika text-xl text-navy ">text ever since the 1500s,
-              when an unknown printer</p>
-        </div>
-
-        <div class="space-x-4 flex">
-          <div class="h-[70vh] w-[25vw] flex-shrink-0">
-            <img src="../assets/hubs.jpg" class="w-full h-full object-cover">
-          </div>
-          <div class="h-[70vh] w-[25vw] flex-shrink-0">
-            <img src="../assets/srs.jpg" class="w-full h-full object-cover">
-          </div>
-        </div>
-        
-       
-      </div>
-
-      <!--------section seven------->
-      <div class="h-full">
-        <img src="../assets/group.jpg" alt="" class=" w-full">
-      </div>
+        <!---------------- section nine ----------->
 
 
-  </div>
-</template>
+    </div>
+  </template>
 
-<script>
-import NavBar from '@/components/Navbar.vue';
-export default {
-  name: 'HomePage',
-  components: {
-    NavBar
+  <script>
+  import NavBar from '@/components/Navbar.vue';
+  export default {
+    name: 'HomePage',
+    components: {
+      NavBar
+    }
   }
-}
-</script>
+  </script>
 
