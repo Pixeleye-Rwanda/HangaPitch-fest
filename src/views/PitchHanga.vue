7bf4b2ecc01ead0f97f8c82932e4c87485abc747
belladev250 <lissadev17@gmail.com>
2024-09-18 11:53:32 +0200

    done with pitch at hanga

--- src/views/PitchHanga.vue
+++ src/views/PitchHanga.vue
@@ -50,34 +50,34 @@
 
             <div class="  absolute flex ml-12  space-x-12 mt-16">
               <div class="w-[350px] h-[40vh] space-y-4">
-                <img src="../assets/one.svg" class="absolute -mt-28 h-13 w-13">
-                <p class="font-rockinsoda text-5xl text-navy"> Create Acount</p>
-                <p class="font-gramatika text-xl  text-darkGray"> Sign up on Hanga by clicking on the 
+                <img src="../assets/one.svg" class="absolute  -mt-24 h-16 w-16">
+                <p class="font-rockinsoda text-4xl text-navy"> Create Acount</p>
+                <p class="font-gramatika text-lg  text-darkGray"> Sign up on Hanga by clicking on the 
                   <a href="https://hanga.acceleratorapp.co/application/new?program=hanga-pitchfest-" class="text-tangerine ">
                     official link to registration</a></p>
               </div>
               <div class="w-[350px] h-[40vh] space-y-4">
-                <img src="../assets/two.svg" class="absolute -mt-28 h-13 w-13">
-                <p class="font-rockinsoda text-5xl text-navy"> Validate Email</p>
-                <p class="font-gramatika text-xl text-darkGray">Check your email inbox for a message from Hanga Pitchfest.
+                <img src="../assets/two.svg" class="absolute - -mt-24 h-16 w-16">
+                <p class="font-rockinsoda text-4xl text-navy"> Validate Email</p>
+                <p class="font-gramatika text-lg text-darkGray">Check your email inbox for a message from Hanga Pitchfest.
                   Open the email and click on the validation link provided</p>
               
               </div>
               <div class="space-y-8 ">
               <div class="w-[350px]  h-[40vh] mr-12 space-y-4">
-                <img src="../assets/three.svg" class="absolute -mt-32 h-13 w-13">
+                <img src="../assets/three.svg" class="absolute  -mt-28 h-16 w-16">
 
-                <p class="font-rockinsoda text-5xl text-navy"> Fill Form</p>
-                <p class="font-gramatika text-xl text-darkGray">Log in to your Hanga Pitchfest account.
+                <p class="font-rockinsoda text-4xl text-navy"> Fill Form</p>
+                <p class="font-gramatika text-lg text-darkGray">Log in to your Hanga Pitchfest account.
                   Complete the application form with accurate details about your startup and upload 
                   any required attachments</p>
               
               </div>
               <div class="w-[350px] h-[40vh]  ">
-                <img src="../assets/four.svg" class="absolute -mt-32 h-13 w-13">
+                <img src="../assets/four.svg" class="absolute -mt-28 h-16 w-16">
                   <div class="space-y-4">
-                <p class="font-rockinsoda text-5xl text-navy mt-24"> submit Project</p>
-                <p class="font-gramatika text-xl text-darkGray">Review your application form to ensure all information is accurate and complete.
+                <p class="font-rockinsoda text-4xl text-navy mt-24"> submit Project</p>
+                <p class="font-gramatika text-lg text-darkGray">Review your application form to ensure all information is accurate and complete.
                   Once you're satisfied with the application, click the "Submit" button.</p>
                 </div>
               </div>
@@ -174,6 +174,82 @@
                 
         </div>
         </div>
+
+        <!-- another section -->
+        <div class="ml-12 space-x-32 mt-54 flex mb-16 ">
+          <div class="mt-32 ">
+            <div class="space-y-2 mt-28">
+            <p class="text-5xl font-rockinsoda text-navy">CONNECT WITH LIKE-MINDED</p>
+            <p class="text-5xl font-rockinsoda text-navy">ENTREPRENEURS AT HANGA HUBs</p>
+          </div>
+
+          <div class="mt-8">
+            <div class=" text-justify space-y-8 w-[600px] ">
+              <p class="text-navy font-gramatika text-md">HANGA Hubs is an action funded by the European Commission
+                 and it is implemented by RISA. It operates in MUHANGA, NYAGATARE, RUBAVU and RUSIZI. 
+                 The action is part of the Rwanda Government’s and the European Union’s efforts to support the private sector development
+                 and job creation in Rwanda through Innovation and Incubation hubs.</p>
+
+                 <button class="mr-8 mt-10 p-2 px-8 font-gramatika bg-tangerine text-white font-semibold zoom-in button-hover" ref="button">
+                  Learn more <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                </button>
+
+             
+         </div>
+          </div>
+        </div>
+
+          <div class=" relative h-[520px] mt-28 flex-shrink-0">
+            <img src="../assets/vr.jpg" class="w-full h-full object-cover ">
+            <div class="absolute inset-0" 
+            style="background: linear-gradient(to bottom, rgba(1, 9, 48, 0.05) 0%, rgba(1, 9, 48, 0.2) 50%, rgba(1, 9, 48, 0.8) 100%);">
+            </div>
+
+          </div>
+
+        </div>
+
+
+        <!-- next section -->
+        <div class="relative">
+          <!---------section 12------->
+          <div class="h-[400px] relative z-10 -mb-48"> 
+            <div class="absolute w-full left-0 right-0 mt-20">
+              <img src="../assets/footersvg.svg" alt="" class="w-full h-auto block">
+              <img src="../assets/hanga elements_illustration 2.svg" class="absolute top-0 mt-24 left-[57%] w-1/3 h-auto">
+            </div>
+            
+            <div class="absolute mt-44 ml-2 p-8 text-left">
+              <h2 class="text-6xl text-white font-rockinsoda mb-4">NEVER MISS</h2>
+              <h2 class="text-6xl text-white font-rockinsoda mb-4">A HIGHLIGHT FROM HANGA</h2>
+              <p class="text-lg mb-1 text-white text-xl font-gramatika">
+                Get the latest trends from Hanga and beyond with our weekly
+              </p>
+              <p class="text-lg mb-1 text-white text-xl font-gramatika">
+                roundup.
+              </p>
+              
+              <div class=" -ml-6 p-4 rounded flex">
+                <input
+                  type="email"
+                  placeholder="Your email address"
+                  class="bg-ftcolor text-gray-400 placeholder-gray-500 focus:outline-none px-12 py-2 rounded-l w-[300px]"
+                />
+                <a href="" class="bg-orange-600 hover:bg-orange-700 text-white px-4 py-2 rounded-r">
+                  <i class="fa fa-chevron-right" aria-hidden="true"></i>
+
+
+                </a>
+              </div>
+              
+            </div>
+          </div>
+          
+          <!------section 13 (Footer)--------- -->
+          <div class="relative z-0 mt-[400px]">
+            <FooterPage/>
+          </div>
+        </div>
             
         
       </div>
@@ -182,10 +258,12 @@
 
   <script>
   import NavBar from '@/components/Navbar.vue';
+  import FooterPage from '@/components/footerPage.vue';
   export default{
       name:"PitchHanga",
       components:{
-          NavBar
+          NavBar,
+          FooterPage
       },
 
       data(){
@@ -248,6 +326,7 @@
                     observer.observe(elements[key]);
                 }
                 }
+                
          
             }
 
